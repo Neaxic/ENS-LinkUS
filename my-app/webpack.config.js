6a434c1ae631f42f060deadcd129632d2fdbb0c5
Andre <andreas79001@gmail.com>
2022-07-06 16:03:12 +0200

    ad

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -14,18 +14,18 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     fallback: {
-        fs: false,
-        tls: false,
-        net: false,
-        path: false,
-        zlib: false,
-        http: false,
-        https: false,
-        stream: false,
-        crypto: false,
-        url: false,
-        os: false,
-        assert: false
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+        "url": false,
+        "os": false,
+        "assert": false
       }
   },
   output: {
